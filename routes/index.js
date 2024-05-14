459f1dd278fe0064e00dd03e0dbfcfd976086237
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-05-15 00:39:16 +0300

    2. First API

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
