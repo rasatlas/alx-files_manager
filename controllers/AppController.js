1118c5d9a113eb592728261208c8668e54fc6104
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-05-15 00:59:11 +0300

    2. First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,7 +3,9 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    response
+      .status(200)
+      .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(request, response) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
+/* eslint-disable jest/require-hook */
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
